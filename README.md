d6dec0024cdf12321355f018b3a20dda8e9e8ab8
sanjayjohn <sanjayjohn@Sanjay-Johns-MacBook-Pro.local>
2018-07-03 10:42:54 -0400

    First Commit

---
+++ README.md
@@ -0,0 +1,53 @@
+#  NUnit Automation with updating existing Manual Test Cases
+
+## Overview:
+
+In the function.py script, there is a function called updateManualTestCase. The method will query for a test case, and if the manual test case exists, the method will update the test case to an automated one. This method should be used if you want to convert your manual test cases to automatic ones so that the old test cases can be used with the auto test log API. If the test case does not exist, the API will create test cases.
+
+While parsing through the TestResult.xml results file, the updateManualTestCase method will be called, so that existing test cases with the same name as the class name in the xml document can converted to automated ones.
+
+Note: The updateManualTestCase method can be used with any type of parser
+
+
+### From Command Prompt:
+
+1. Make sure pip was installed correctly with python on your machine by running the following command. It should output the pip version:
+
+ `pip --version`
+
+ Note: pip3 will work as well. Try `pip3 --version`
+
+2. If pip is not installed, run the following command to install pip:
+
+ `python -m -ensurepip --default-pip`
+
+More information about downloading pip can be found at [https://packaging.python.org/tutorials/installing-packages/](https://packaging.python.org/tutorials/installing-packages/)
+
+3. After you have ensured pip is installed, run the following commands individually :
+
+`pip install requests`
+
+`pip install beautifulsoup4`
+
+`pip install lxml`
+
+`pip install pybase64`
+
+Note: If using pip3 run commands with pip3 instead e.g. `pip3 install requests`
+
+These commands will install the necessary modules required to run the python scripts. The modules are used to send requests to the API, read json configuration files, parse xml documents, and upload files to qTest.
+
+
+## Update Configuration File (conf.json):
+
+**git\_url:** The shell script uses the url to clone a repository and send pull requests everytime it runs if -g input is used
+
+**local\_repository:** The folder containing the Selenium C Sharp. The shell script will use this to know where to run the nunit tests. To run the nunit tests, the shell script looks for .dll file which is located in bin\Debug. For the script to work the local repository folder name must be the same as the .dll file that gets created when the project gets built.
+
+**qtest\_api\_token:** The token used to authorize the connection to qTest Manager
+
+**qtest\_url:** The personal url that is used to access QASymphony API
+
+**project\_id:** The Project Id 
+
+Open the conf.json file and update with your personal information. Enter your own qTest URL and API Token found in the qTest Manager Environment.
---
+++ functions.py
@@ -0,0 +1,257 @@
+import json
+import pathlib
+import requests
+import sys
+import time
+import base64
+
+
+def get_config():
+    # Check to ensure the configuration file exists and is readable.
+    try:
+        path = pathlib.Path("conf.json")
+        if path.exists() and path.is_file():
+            with open(path) as config_file:
+                try:
+                    qtest_config = json.load(config_file)
+                    return qtest_config
+                except json.JSONDecodeError:
+                    print("Error: Configuration file not in valid JSON format.")
+        else:
+            raise IOError
+    except IOError:
+        print("Error: Configuration file not found or inaccessible.")
+        return -1
+    except Exception as e:
+        print("Error: Unexpected error loading configuration: " + str(e))
+        return -1
+
+
+def create_test_logs_json(runName, stepName, status, note, log, start, end):
+    attachment = []
+    if start is None:
+        start = time.strftime('%Y-%m-%dT%H:%M:%SZ', time.gmtime())
+    if end is None:
+        end = time.strftime('%Y-%m-%dT%H:%M:%SZ', time.gmtime())
+    if log is not '':
+        value = {'name': stepName, 'content_type': 'application/json', 'data': log}
+        attachment.append(value)
+    body = {'exe_start_date': start,
+            'exe_end_date': end,
+            'note': note,
+            'name': runName,
+            'status': status,
+            'automation_content': runName + '#' + stepName,
+            'attachments': attachment,
+            'module_names': [runName, stepName]
+            }
+    return body
+
+def updateManualTestCase(className, methodName):
+    qtest_config = get_config()
+    api_token = qtest_config["qtest_api_token"]
+    qTestUrl = qtest_config["qtest_url"]
+    projectId = qtest_config["project_id"]
+    baseUrl = '{}/api/v3/projects/{}/search'
+
+    payload = {
+      "object_type": "test-cases",
+      "fields": [
+        "*"
+      ],
+    "query": "'name' ~ " + className
+    }
+
+    key = '{}'
+    key = key.format(api_token)
+    headers = {'Content-Type': 'application/json',
+               "Authorization": key}
+
+    queryUrl = baseUrl.format(qTestUrl, projectId)
+    testCaseID = None
+    try:
+        r = requests.post(queryUrl, data=json.dumps(payload), headers=headers)
+        testCase = json.loads(r.content)
+        testCaseID = testCase['items'][0]['id']
+    except:
+        print("Unable to find Test Case.")
+
+    if testCaseID is not None:
+        baseUrl = '{}/api/v3/projects/{}/test-cases/{}'
+        testCaseUrl = baseUrl.format(qTestUrl, projectId, testCaseID)
+        key = '{}'
+        key = key.format(api_token)
+        headers = {'Content-Type': 'application/json',
+                   "Authorization": key}
+        testCaseJson = None
+        try:
+            r = requests.get(testCaseUrl, headers=headers)
+            testCaseJson = json.loads(r.content)
+            # print(testCaseJson)
+        except:
+            print("Error: Unable to find Test Case.")
+
+        if testCaseJson is not None:
+            updateRequired = False
+            for field in testCaseJson['properties']:
+                if field['field_name'] == 'Automation' and field['field_value'] != '711':
+                    field['field_value'] = '711'
+                    field['field_value_name'] = 'Yes'
+                    updateRequired = True
+                if field['field_name'] == 'Automation Content' and field['field_value'] == '':
+                    field['field_value'] = testCase + '#' + methodName
+            # print(testCaseJson)
+            if updateRequired:
+                try:
+                    r = requests.put(url=testCaseUrl, data=json.dumps(testCaseJson),
+                                     headers=headers)
+                    print(r.content)
+                except:
+                    print("Unable to update testcase")
+
+
+
+def parse_xml():
+    try:
+        from BeautifulSoup import BeautifulSoup
+    except ImportError:
+        from bs4 import BeautifulSoup
+    try:
+        file = './TestResult.xml'
+        xml = open(file)
+    except:
+        print('Error: Enter a valid file path argument or file not found or inaccessible')
+        return -1
+    body = []
+    soup = BeautifulSoup(xml, 'xml')
+    message = ''
+    log = ''
+    for tag in soup.find_all('test-case'):
+        try:
+            runName = tag['classname']
+            stepName = tag['methodname']
+        except:
+            runName = tag['fullname']
+            stepName = tag['name']
+        updateManualTestCase(runName, stepName)
+        status = tag['result']
+        if tag.find('start-time') is not None and tag.find('end-time') is not None:
+            start = tag['start-time']
+            end = tag['end-time']
+        else:
+            start = None
+            end = None
+        if status == "Failed":
+            status = "FAIL"
+        elif status == "Passed":
+            status = "PASS"
+        else:
+            status = "SKIP"
+        if status == 'FAIL':
+            if tag.find('stack-trace') is not None:
+                log = tag.find('stack-trace').text
+                log = base64.b64encode(bytes(log, 'utf-8'))
+                log = log.decode('utf-8')
+            if tag.find('message') is not None:
+                message = tag.find('message').text
+        else:
+            message = ''
+            log = ''
+        value = create_test_logs_json(runName, stepName, status, message, log, start, end)
+        body.append(value)
+    return body
+
+
+def post_all_tests():
+    qtest_config = get_config()
+    api_token = qtest_config["qtest_api_token"]
+    qTestUrl = qtest_config["qtest_url"]
+    projectId = qtest_config["project_id"]
+    body = parse_xml()
+
+    baseUrl = '{}/api/v3/projects/{}/auto-test-logs'
+
+    testLogUrl = baseUrl.format(qTestUrl, projectId)
+    try:
+        update = sys.argv[2]
+        if update == 'update':
+            testCycle = get_test_cycle()
+        else:
+            testCycle = post_test_cycle()
+    except:
+        print("Error: Enter valid argument (true or false) to update existing test cycle or not")
+        return -1
+    payload = {
+        'skipCreatingAutomationModule': False,
+        'test_logs': body,
+        'execution_date': time.strftime('%Y-%m-%dT%H:%M:%SZ', time.gmtime()),
+        'test_cycle': testCycle
+    }
+
+    key = '{}'
+    key = key.format(api_token)
+    headers = {'Content-Type': 'application/json',
+           "Authorization": key}
+    params = {'type': 'automation'}
+    try:
+        r = requests.post(testLogUrl, params=params, data=json.dumps(payload), headers=headers)
+        print(r.text)
+    except:
+        print("Error: Unable to post data to qTest Manager API.")
+        return -1
+
+def post_test_cycle():
+    qtest_config = get_config()
+    api_token = qtest_config["qtest_api_token"]
+    qTestUrl = qtest_config["qtest_url"]
+    projectId = qtest_config["project_id"]
+
+    baseUrl = '{}/api/v3/projects/{}/test-cycles/'
+
+    testLogUrl = baseUrl.format(qTestUrl, projectId)
+    payload = {
+        "id": 1,
+        "name": "Selenium Automated Tests",
+        'last_modified_date': time.strftime('%Y-%m-%dT%H:%M:%SZ', time.gmtime())
+    }
+
+    key = '{}'
+    key = key.format(api_token)
+    headers = {'Content-Type': 'application/json',
+           "Authorization": key}
+
+    r = requests.post(testLogUrl, data=json.dumps(payload), headers=headers)
+    string = json.loads(r.text)
+    testcycleId = string.get("id")
+    return testcycleId
+
+def get_test_cycle():
+    qtest_config = get_config()
+    api_token = qtest_config["qtest_api_token"]
+    qTestUrl = qtest_config["qtest_url"]
+    projectId = qtest_config["project_id"]
+
+    baseUrl = '{}/api/v3/projects/{}/test-cycles/'
+
+    testLogUrl = baseUrl.format(qTestUrl, projectId)
+    payload = {
+        "id": 1,
+        "name": "Features",
+        'last_modified_date': time.strftime('%Y-%m-%dT%H:%M:%SZ', time.gmtime()),
+    }
+
+    key = '{}'
+    key = key.format(api_token)
+    headers = {'Content-Type': 'application/json',
+           "Authorization": key}
+
+    r = requests.get(testLogUrl, data=json.dumps(payload), headers=headers)
+    string = json.loads(r.text)
+    testcycleId = None
+    for attrib in string:
+        name = attrib.get('name')
+        if name == "Selenium Automated Tests":
+            testcycleId = attrib.get('id')
+    if testcycleId is None:
+        testcycleId = post_test_cycle()
+    return testcycleId
